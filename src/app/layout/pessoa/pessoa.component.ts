04b0e3c494836dcad3a9acec6d887f779c2c740f
AndreiBack <andreigregoriback@gmail.com>
2023-10-03 21:40:39 -0300

    terminado

--- src/app/layout/pessoa/pessoa.component.ts
+++ src/app/layout/pessoa/pessoa.component.ts
@@ -25,9 +25,39 @@ export class PessoaComponent {
     pessoa4.nome = "Minha vó";
     pessoa4.idade = 80;
 
+    let pessoa5:Pessoa= new Pessoa();
+    pessoa5.nome = "Gertrudes";
+    pessoa5.idade = 40;
+
+    let pessoa6:Pessoa= new Pessoa();
+    pessoa6.nome = "Mulher";
+    pessoa6.idade = 40;
+
+    let pessoa7:Pessoa= new Pessoa();
+    pessoa7.nome = "Rato do esgoto";
+    pessoa7.idade = 32;
+
+    let pessoa8:Pessoa= new Pessoa();
+    pessoa8.nome = "Marcelo autopeças";
+    pessoa8.idade = 31;
+    
+    let pessoa9:Pessoa= new Pessoa();
+    pessoa9.nome = "Reyna";
+    pessoa9.idade = 41;
+    
+    let pessoa10:Pessoa= new Pessoa();
+    pessoa10.nome = "Pedrinho games";
+    pessoa10.idade = 14;
+
     this.lista.push(pessoa1);
     this.lista.push(pessoa2);
     this.lista.push(pessoa3);
     this.lista.push(pessoa4);
+    this.lista.push(pessoa5);
+    this.lista.push(pessoa6);
+    this.lista.push(pessoa7);
+    this.lista.push(pessoa8);
+    this.lista.push(pessoa9);
+    this.lista.push(pessoa10);
   }
 }
